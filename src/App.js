188edd65619b503de2194822a01e3104b49a0626
thathsarani shenali <shenalithath@gmail.com>
2023-08-22 16:23:25 +0530

    upated the code with axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
-import Header from "./components/Header";
-import TodoList from "./components/TodoList";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
